6900182ea868cd5c01517cdff94dc437a0e5c792
prabhakar <prabhakar.boddukuri@achiever-90.local>
2018-05-07 14:04:59 +0530

    updated

--- TableViewContactWithImage/ViewController.swift
+++ TableViewContactWithImage/ViewController.swift
@@ -8,18 +8,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    @IBOutlet weak var profileImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func uploadImageAction(_ sender: Any) {
+   
+    let imageController = UIImagePickerController()
+        imageController.delegate = self
+        imageController.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        self.present(imageController, animated: true, completion: nil)
+    
+    
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        profileImageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func SaveAction(_ sender: Any) {
+        
+        let alertController = UIAlertController(title: "Add New Name", message: "", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addTextField { (textField : UITextField!) -> Void in
+            textField.placeholder = "Enter Name"
+            textField.keyboardType = UIKeyboardType.namePhonePad
+        }
+        let saveAction = UIAlertAction(title: "Save", style: UIAlertActionStyle.default, handler: { alert -> Void in
+            let firstTextField = alertController.textFields![0] as UITextField
+            let secondTextField = alertController.textFields![1] as UITextField
+            
+            let tvc = self.storyboard?.instantiateViewController(withIdentifier: "abc") as! ContactsTableViewController
+        
+            
+            
+            
+       //     self.firstNameTF.text = firstTextField.text
+       //     self.lastNameTF.text = secondTextField.text
+        })
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.default, handler: {
+            (action : UIAlertAction!) -> Void in })
+        alertController.addTextField { (textField : UITextField!) -> Void in
+            textField.placeholder = "Enter Phone Number"
+            textField.keyboardType = UIKeyboardType.phonePad
+        }
+        
+        alertController.addAction(saveAction)
+        alertController.addAction(cancelAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+    
 }
 
